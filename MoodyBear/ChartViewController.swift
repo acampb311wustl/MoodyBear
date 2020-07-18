1d2b3b6b236207e11dc9cba492522459e40c1063
Adam Campbell <acampb311@outlook.com>
2020-07-18 13:25:41 -0500

    continuing to hook things up

--- MoodyBear/ChartViewController.swift
+++ MoodyBear/ChartViewController.swift
@@ -13,57 +13,42 @@ class ChartViewController: UIViewController {
     @IBOutlet weak var number1: UISlider!
     @IBOutlet weak var number2: UISlider!
     @IBOutlet weak var number3: UISlider!
-    @IBOutlet weak var pieChart: PieChartView!
     @IBOutlet weak var barChart: LineChartView!
     
     
     @IBAction func renderCharts() {
         barChartUpdate()
-        pieChartUpdate()
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         barChartUpdate()
-        pieChartUpdate()
     }
     
     func barChartUpdate () {
-     
-        let dollars1 = [20.0, 4.0, 6.0, 3.0, 12.0, 16.0, 4.0, 18.0, 2.0, 4.0, 5.0, 4.0]
-        let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+        let currentLevels = MoodDatabase.db.selectAllFromDatabase()
+
         // 1 - creating an array of data entries
         var yValues : [ChartDataEntry] = [ChartDataEntry]()
 
-        for i in 0 ..< months.count {
-            yValues.append(ChartDataEntry(x: Double(i + 1), y: dollars1[i]))
+        for i in 0 ..< currentLevels.count {
+            yValues.append(ChartDataEntry(x: Double(i + 1), y: Double(currentLevels[i].level)))
         }
 
         let data = LineChartData()
-        let ds = LineChartDataSet(entries: yValues, label: "Months")
+        let ds = LineChartDataSet(entries: yValues)
         data.addDataSet(ds)
         barChart.data = data
     
         //This must stay at end of function
         barChart.notifyDataSetChanged()
-        
     }
-    func pieChartUpdate () {
-        let entry1 = PieChartDataEntry(value: Double(number1.value), label: "#1")
-        let entry2 = PieChartDataEntry(value: Double(number2.value), label: "#2")
-        let entry3 = PieChartDataEntry(value: Double(number3.value), label: "#3")
-        let dataSet = PieChartDataSet(entries: [entry1, entry2, entry3], label: "Widget Types")
-        let data = PieChartData(dataSet: dataSet)
-        pieChart.data = data
-        pieChart.chartDescription?.text = "Share of Widgets by Type"
-        
-        //All other additions to this function will go here
-        
-        //This must stay at end of function
-        pieChart.notifyDataSetChanged()
-        
+    
+    override func viewWillAppear(_ animated: Bool) {
+        barChartUpdate()
     }
+
     
 }
 
--- MoodyBear/DataEntryController.swift
+++ MoodyBear/DataEntryController.swift
@@ -10,52 +10,25 @@ import UIKit
 
 class DataEntryController: UIViewController {
     @IBOutlet weak var moodLevelSlider: UISlider!
-    
     @IBOutlet weak var moodDetailsField: UITextView!
-    
-    var moodDB : FMDatabase!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        
-//        loadDatabase()
     }
     
-    func loadDatabase() {
-        let thepath = Bundle.main.path(forResource: "moods", ofType: "db")
-        
-        moodDB = FMDatabase(path: thepath)
-        
-        if !(moodDB.open()) {
-            print("unable to open db!")
-            return
-        }
-    }
+    
     @IBAction func submitButtonAction(_ sender: Any) {
-
-//        do {
-//            let query = "insert into moodHistory (level,description) values (3,'\(moodDetailsField.text ?? "")')"
-//            try moodDB.executeUpdate(query, values:nil)
-//
-////            insert into moodHistory (level,description) values (1, 'not feeling so great :(');
-//
-//        } catch let error as NSError {
-//            print("db error \(error)")
-//        }
+        let tempNum = Int(moodLevelSlider.value)
+        let tempDetails = moodDetailsField.text ?? ""
+        
+        MoodDatabase.db.insertIntoDatabase(level: tempNum, description: tempDetails)
     }
     
     @IBAction func clearButtonAction(_ sender: Any) {
-//        do {
-//            let results = try moodDB.executeQuery("select * from moodHistory", values: nil)
-//
-//            while (results.next()) {
-//                let someMood = results.string(forColumn: "description") ?? ""
-//                print(String(someMood))
-//            }
-//        } catch let error as NSError {
-//            print("db error \(error)")
-//        }
+        let asdf = MoodDatabase.db.selectAllFromDatabase()
+        for temp in asdf {
+            print(temp.description)
+        }
     }
     
 }
