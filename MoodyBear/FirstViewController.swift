d2274b5154a39f575ec3cee6581a27b3cd609c6a
Adam Campbell <acampb311@outlook.com>
2020-07-18 08:09:44 -0500

    Initial Commit

---
+++ MoodyBear/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  MoodyBear
+//
+//  Created by Adam Campbell on 7/18/20.
+//  Copyright © 2020 Team Moody Bear. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MoodyBear/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  MoodyBear
+//
+//  Created by Adam Campbell on 7/18/20.
+//  Copyright © 2020 Team Moody Bear. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
